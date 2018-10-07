baad0c2d79a7d2e2bf84aa5f912f3a6ac082005f
vladislavmakarevich <bgmakar79@gmail.com>
2018-10-07 21:51:24 +0300

    feat:

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+  for(let i=0;i<9;i++){
+		for(let j=0;j<9;j++){
+			if(matrix[i][j]===0){
+				matrix[i][j]=[];
+			}
+		}
+	}
+	return matrix;
 }
+
+	
